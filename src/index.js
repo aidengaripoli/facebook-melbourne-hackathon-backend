be95e4d11bb9a4435e04525b518376f0dc945394
Yingcong Zhu From Mac <yingcongzhu@gmail.com>
2019-05-04 21:22:38 +1000

    roughly a skeleton

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer');
+// const https = require("https");
+const axios = require('axios');
 
 const app = express()
 
@@ -24,5 +27,44 @@ app.post('/generate', (req, res) => {
   console.log(dates, criteria, people)
   res.status(200).json({ message: [dates, criteria, people] })
 })
+
+app.post('/email', async (req, res) => {
+  let testAccount = await nodemailer.createTestAccount();
+  let transporter = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "aab2cd4f5dce36",
+      pass: "c3478de1b31297"
+    }
+  });
+  let info = await transporter.sendMail({
+    from: '"Fred Foo " <foo@example.com>', // sender address
+    to: "29fe75dbfe-0ea057@inbox.mailtrap.io", // list of receivers
+    subject: "Hello", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>" // html body
+  }).then(() => {
+    res.send({'message': 'success'});
+    console.log("lol");
+  });
+});
+
+// should call with GET / instead
+app.get('/weather', (res, req) => {
+    // key = "e0089bb5191cc5c929574c9f816bba5b";
+    // request = "https://api.openweathermap.org/data/2.5/weather?q=Melbourne&appid=" + key;
+    request = "https://api.darksky.net/forecast/376d0c405226b9b108c454f5186a72c8/37.8267,-122.4233"
+    // change the latitude and longtitude later
+    axios.get(request).then(res => {return res.data}).then(data => {
+      // console.log(data.daily.data[0].precipType)
+      let forecasts = data.daily.data;
+      forecasts.forEach(data => {
+        // candidate type rain, snow, sleet
+        // if null, good weather
+        console.log(data.precipType);
+      })
+    })
+});
 
 app.listen(3000, () => console.log('listening on port 3000'))
