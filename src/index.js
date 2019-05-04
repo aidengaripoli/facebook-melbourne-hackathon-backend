2fd210a943a612f5821f85cd299bbd1aef780562
Aiden Garipoli <aidosgaripoli@gmail.com>
2019-05-04 18:37:52 +1000

    added google apis and some maps searching logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const moment = require('moment')
 
 const app = express()
 
@@ -15,14 +16,103 @@ mongoose.connection.on('error', (err) => {
   process.exit()
 })
 
+const googleMapsClient = require('@google/maps').createClient({
+  key: 'AIzaSyBtz626NHTfso4tPcJJE2t8rSW3H96heUk',
+  Promise: Promise
+})
+
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'api' })
 })
 
-app.post('/generate', (req, res) => {
-  const { dates, criteria, people } = req.body
-  console.log(dates, criteria, people)
-  res.status(200).json({ message: [dates, criteria, people] })
+app.post('/generate', async (req, res) => {
+  const { startTimestamp, endTimestamp, location, criteria, people } = req.body
+  // location -> { cityName: '', cityLatLong: ',' }
+  console.log(startTimestamp, endTimestamp, criteria, people)
+  
+  // dates
+  const startDate = moment(startTimestamp)
+  const endDate = moment(endTimestamp)
+  const days = endDate.diff(startDate, 'days', false)
+
+  // hotel
+  if (days > 1) {
+    // need a hotel
+  }
+
+  // nearby food
+  const lunchRestaurant = await getNearbyRestaurant(location.cityLatLong)
+  const dinnerRestaurant = await getNearbyRestaurant(location.cityLatLong)
+
+  console.log(lunchRestaurant)
+
+  // categories keywords
+  const keywords = {
+    romantic: ['beaches', 'cinemas', 'gardens'],
+    sport: ['stadiums', 'arenas'],
+    nature: [],
+    historic: []
+  }
+
+  const middayevent = await getNearbyPlace(`${keywords[criteria]} in ${location.cityName}`)
+
+  // return the plan
+  res.status(200).json({
+    plan: [
+      {
+        // morningevent: null,
+        lunch: {
+          name: lunchRestaurant.name,
+          rating: lunchRestaurant.rating
+        },
+        middayevent: null,
+        dinner: {
+          name: dinnerRestaurant.name,
+          rating: dinnerRestaurant.rating
+        },
+        hotel: null // if required
+      }
+    ]
+  })
 })
+
+// functions
+function getNearbyPlace(query, location) {
+  return googleMapsClient.places({
+    query,
+    // location: '-37.8207879,144.9561307',
+    location,
+    radius: 500
+  })
+  .asPromise()
+  .then((response) => {
+    return response.json.results[0]
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+}
+
+function getNearbyRestaurant(location) {
+  return googleMapsClient.placesNearby({
+    // location: '-37.8207879,144.9561307',
+    location,
+    radius: 500,
+    type: 'restaurant',
+    name: 'restaurant'
+  })
+  .asPromise()
+  .then((response) => {
+    // console.log(response.json.results.length)
+    let index = Math.floor(Math.random() * (response.json.results.length - 0) + 0)
+    console.log(index)
+    // response.json.results[index].name
+    // console.log(response.json.results[index].name)
+    return response.json.results[index]
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+}
 
 app.listen(3000, () => console.log('listening on port 3000'))
