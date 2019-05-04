a78a521217af4d9e95458ad3e6736fb5a7419b99
Aiden Garipoli <aidosgaripoli@gmail.com>
2019-05-04 14:20:21 +1000

    add mongodb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+// mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect('mongodb://demo:demoadmin123@ds149596.mlab.com:49596/fb-hackathon', { useNewUrlParser: true })
+mongoose.connection.on('error', (err) => {
+  console.error(err)
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'))
+  process.exit()
+})
 
 app.use('/', (req, res) => {
   res.status(200).json({ message: 'working' })
